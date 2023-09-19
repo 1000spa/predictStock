7794d3d2a1c7d1c866110bc775e5f1f2ccf80d89
unknown <shinepyo@naver.com>
2023-09-19 22:36:53 +0900

    왜 업로드 안됨

---
+++ program/main.py
@@ -0,0 +1,3 @@
+from predict import deep
+filename = 'test_down.png'
+print(deep(f'./test_image/{filename}'))
--- web/main.py
+++ web/main.py
@@ -16,7 +16,10 @@ def predict():
     filename = str(secure_filename(f.filename))
     basedir = os.path.abspath(os.path.dirname(__file__))
     f.save(str(os.path.join(application.config['UPLOAD_FOLDER'])+"/"+filename))
-    return render_template('result.html', result=deep(f'./img/{filename}'))
+    result = deep(f'./img/{filename}')
+    confidence = round(result['confidence'] * 100,2)
+    os.remove(str(os.path.join(application.config['UPLOAD_FOLDER'])+"/"+filename))
+    return render_template('result.html', result=f"{result['class']}, {confidence}%")
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=80, debug=True)
+    application.run(host='0.0.0.0', port=5000, debug=True)
